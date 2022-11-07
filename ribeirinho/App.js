364935f4355752b79a6ff8f2ebf220bd0f968b9d
SannayDuarte <Sannaylinnyd@gmail.com>
2022-11-07 02:35:28 -0300

    commit react

--- ribeirinho/App.js
+++ ribeirinho/App.js
@@ -1,14 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+
+function Clientes({ navigation }) {
+  const [Clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    async function fetchRibeirinhoViagem() {
+      const response = await fetch('http://localhost:3306/ribeirinhomd6/cliente');
+      setClientes(await response.json());
+    }
+    fetchRibeirinhoViagem();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <View style={styles.fixToText}>
+        <Button color='#034C8C'
+          title=" Verificar Clientes de Ribeirinho Viagem"
+          onPress={() => navigation.navigate('Clientes')}
+        />
+      </View>
+      <View style={styles.fixToList}>
+        <Text style={{ width: '10%' }}>Id</Text>
+        <Text style={{ width: '30%' }}>Nome</Text>
+        <Text style={{ width: '30%' }}>Sobrenome</Text>
+        <Text style={{ width: '30%' }}>Rg</Text>
+        <Text style={{ width: '30%' }}>Cpf</Text>
+        <Text style={{ width: '30%' }}>Data De Nascimento</Text>
+        <Text style={{ width: '30%' }}>Celular</Text>
+        <Text style={{ width: '30%' }}>Sexualidade</Text>
+        <Text style={{ width: '30%' }}>Email</Text>
+        <Text style={{ width: '30%' }}>Senha</Text>
+
+        <Text style={{ width: '30%' }}>Cep</Text>
+        <Text style={{ width: '30%' }}>Estado</Text>
+        <Text style={{ width: '30%' }}>Cidade</Text>
+        <Text style={{ width: '30%' }}>Bairro</Text>
+        <Text style={{ width: '30%' }}>Rua</Text>
+
+        <Text style={{ width: '30%' }}>Cidade de Destino</Text>
+        <Text style={{ width: '30%' }}>Cidade Origem</Text>
+        <Text style={{ width: '30%' }}>Data da Viagem</Text>
+
+        <Text style={{ width: '30%' }}>Id Da Passagem</Text>
+        <Text style={{ width: '30%' }}>Metodo de Viagem</Text>
+        <Text style={{ width: '30%' }}>Valor</Text>
+      </View>
+      {Clientes.map(cli => (
+        <View>
+          <View style={styles.fixToList}>
+            <Text style={{ width: '10%' }}>{cli.id}</Text>
+            <Text style={{ width: '30%' }}>{cli.nome}</Text>
+            <Text style={{ width: '30%' }}>{cli.sobrenome}</Text>
+            <Text style={{ width: '30%' }}>{cli.rg}</Text>
+            <Text style={{ width: '30%' }}>{cli.cpf}</Text>
+            <Text style={{ width: '30%' }}>{cli.datadenascimento}</Text>
+            <Text style={{ width: '30%' }}>{cli.celular}</Text>
+            <Text style={{ width: '30%' }}>{cli.sexualidade}</Text>
+            <Text style={{ width: '30%' }}>{cli.email}</Text>
+            <Text style={{ width: '30%' }}>{cli.senha}</Text>
+
+            <Text style={{ width: '30%' }}>{cli.cep}</Text>
+            <Text style={{ width: '30%' }}>{cli.estado}</Text>
+            <Text style={{ width: '30%' }}>{cli.cidade}</Text>
+            <Text style={{ width: '30%' }}>{cli.bairro}</Text>
+            <Text style={{ width: '30%' }}>{cli.rua}</Text>
+
+            <Text style={{ width: '30%' }}>{cli.cidadeDestino}</Text>
+            <Text style={{ width: '30%' }}>{cli.cidadeOrigem}</Text>
+            <Text style={{ width: '30%' }}>{cli.dtViagem}</Text>
+
+            <Text style={{ width: '30%' }}>{cli.idPas}</Text> 
+            <Text style={{ width: '30%' }}>{cli.mdtViagem}</Text>
+            <Text style={{ width: '30%' }}>{cli.valor}</Text>
+          </View>
+          <Separator />
+        </View>
+      ))}
     </View>
   );
+};
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Clientes"
+          component={Clientes}
+          options={{
+            title: 'Ribeirinho Viagem',
+            headerStyle: { backgroundColor: '#034C8C', }, headerTintColor: '#fff'
+          }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +117,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo1: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    marginBottom: 50,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 30,
+  },
+  fixToList: {
+    flexDirection: 'row',
   },
 });
